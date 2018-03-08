b65d91c56a268d53e025fbcca8d5b08528ddf216
Prajyoth <prajyoth.bhandary@gmail.com>
2018-03-07 21:12:59 -0800

    Added functionality of search

--- src/components/read/ReadPage.jsx
+++ src/components/read/ReadPage.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import Modal from 'react-modal';
+import Search from '../search/SearchPage'
 
-const urlRead = () => 'http://localhost:3000/rest/article/all'
+const urlRead = () => 'http://localhost:3000/rest/article/all';
+const searchUrl = () => 'http://localhost:3000/rest/article/search';
 
 class ReadPage extends React.Component {
     constructor(props) {
@@ -10,21 +11,41 @@ class ReadPage extends React.Component {
         this.state = {
             headingNames : [],
             contentmap: new Map(),
-            currentheading: '',
-            modalIsOpen: false
+            searchString: ''
         };
 
-        this.openModal = this.openModal.bind(this);
-        this.closeModal = this.closeModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleChange = this.handleChange.bind(this);
     }
 
-    openModal (curheading) {
-        this.setState({modalIsOpen: true, currentheading: curheading});
-      }
-    
-      closeModal() {
-        this.setState({modalIsOpen: false});
-      }
+    handleChange(event) {
+        this.setState({
+            searchString: event.target.value
+        });
+    }
+
+    handleSubmit(event) {
+        let searchUrlWithParam = searchUrl() + '?searchString=' + this.state.searchString;
+        console.log(searchUrlWithParam);
+        fetch(searchUrlWithParam)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            //let obj = JSON.parse(response);
+            let headinglist = [];
+            let bodylist = new Map();
+            for (let i =0; i<data.length; i++) {
+                headinglist.push(data[i].heading);
+                bodylist.set(data[i].heading, data[i].body);
+            }
+            this.setState({
+                headingNames: headinglist,
+                contentmap: bodylist
+            });
+        })
+
+        event.preventDefault();
+    }
 
     componentDidMount() {
         fetch(urlRead())
@@ -49,23 +70,11 @@ class ReadPage extends React.Component {
             <div>
                 <Link to="/create"> Click here to create </Link>
                 <h1> Read an Article </h1>
-                <ul>
-                    {this.state.headingNames.map((names) =>
-                    <li key={names}>
-                    <Link to="/" onClick={this.openModal.bind(this, names)}> {names} </Link>
-                    </li>
-                    )}
-                </ul>
-                <Modal
-                    isOpen={this.state.modalIsOpen}
-                    onRequestClose={this.closeModal}
-                    contentLabel="Article description"
-                >
-                    <button onClick={this.closeModal}>close</button>
-                    <h2>{this.state.currentheading}</h2>
-                    <p>{this.state.contentmap.get(this.state.currentheading)}</p>
-                </Modal>
-
+                <form onSubmit={this.handleSubmit}>
+                <input name='searchString' type="text" value={this.state.searchString} onChange={this.handleChange} />
+                <input type='submit' value='Search' />
+                <Search headingNames={this.state.headingNames} contentmap={this.state.contentmap} />
+            </form>
             </div>
         );
     }
---
+++ src/components/search/SearchPage.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import Modal from 'react-modal';
+
+class Search extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentheading: '',
+            modalIsOpen: false
+        };
+      
+        this.openModal = this.openModal.bind(this);
+        this.closeModal = this.closeModal.bind(this);
+    }
+
+    openModal (curheading) {
+        this.setState({modalIsOpen: true, currentheading: curheading});
+      }
+    
+      closeModal() {
+        this.setState({modalIsOpen: false});
+      }
+
+    render() {
+        return (
+            <div>
+                 <ul>
+                    {this.props.headingNames.map((names) =>
+                    <li key={names}>
+                    <Link to="/" onClick={this.openModal.bind(this, names)}> {names} </Link>
+                    </li>
+                    )}
+                </ul>
+                <Modal
+                    isOpen={this.state.modalIsOpen}
+                    onRequestClose={this.closeModal}
+                    contentLabel="Article description"
+                >
+                    <button onClick={this.closeModal}>close</button>
+                    <h2>{this.state.currentheading}</h2>
+                    <p>{this.props.contentmap.get(this.state.currentheading)}</p>
+                </Modal>
+
+            </div>
+        )
+    }
+}
+
+export default Search;
